ff85d59ebb48eeccb902a0582114f83b4ac93d5b
Rahul <rahul@gep.com>
2023-07-02 20:47:26 +0530

    User defined exceptions added

---
+++ SalesOrderExceptions/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SalesOrderExceptions
+{
+    class InvalidSalesPersonNameException : ApplicationException
+    {
+        public InvalidSalesPersonNameException(string msg) : base(msg)
+        {
+
+        }
+    }
+    class InvalidCustomerNameException : ApplicationException
+    {
+        public InvalidCustomerNameException(string msg) : base(msg)
+        {
+
+        }
+    }
+    class NoOrdersFoundException : ApplicationException
+    {
+        public NoOrdersFoundException(string msg) : base(msg)
+        {
+
+        }
+    }
+}
