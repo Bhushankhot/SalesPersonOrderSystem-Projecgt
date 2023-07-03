c898db2863f9d3dc2ffe3090d350418ebc07e1ac
Swapnil02k <72118764+Swapnil02k@users.noreply.github.com>
2023-07-03 07:46:14 +0530

    Update Program.cs
    Updated Program file

--- SalesPersonOrderSystem/Program.cs
+++ SalesPersonOrderSystem/Program.cs
@@ -10,8 +10,85 @@ namespace SalesPersonOrderSystem
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world");
-            Console.WriteLine("this is the first commit");
+           Console.WriteLine(" ********* Welcome to CURD App by Team-H ********* ");
+
+            DataAccessCls dao = new DataAccessCls();
+            int ch;
+
+            do
+            {
+            Console.WriteLine("\n1.Add Order ");
+            Console.WriteLine("2.Display Order ");
+            Console.WriteLine("3.Exit ");
+            Console.WriteLine("Enter your choice : ");
+            ch = int.Parse(Console.ReadLine());
+            switch (ch)
+            {
+                case 1://To Add Details in Orders table
+                        var details = new Order();
+                        Console.WriteLine("Enter Order Date (dd-mm-yyyy) : ");
+                        string text = Console.ReadLine();
+                        details.order_date = DateTime.ParseExact(text, "dd/MM/yyyy", null);
+                        Console.WriteLine("Enter Order Amount : ");
+                        details.Amount=int.Parse(Console.ReadLine());
+                        Console.WriteLine("Enter Customer Name : ");
+                        details.Customer.Cname=Console.ReadLine();
+                        Console.WriteLine("Enter Sales Person Name : ");
+                        details.SalesPerson.Sname=Console.ReadLine();
+                        dao.AddOrder(details);
+                        Console.WriteLine("Record Inserted .... ");
+                    break;
+                case 2:// To Display Details based on SalesPerson Name
+                        var lstOrders = dao.GetOrders();
+                            Console.WriteLine($"Order_id\tAmount\torder_date\tsalesperson_id\tcust_id");
+                        foreach (var item in lstOrders)
+                        {
+                            Console.WriteLine($"\t{item.Order_id}\t{item.Amount}\t{item.order_date}\t{item.salesperson_id}\t{item.cust_id}");
+                        }
+                    break;
+                case 3://Exit
+                        Console.WriteLine("Thank you for using :) ");
+                        ch = 3;
+                    break;
+                default:
+                        Console.WriteLine("Please enter valid choice !!!");
+                    break;
+            }
+            } while (ch!=3);
         }
+    }
+    class DataAccessCls
+    {
+        public List<Order> GetOrders()
+        {
+            var dbctx = new CURDBEntities();
+            var result = dbctx.Orders.ToList();
+            return result;
+        }
+        public Order GetOrder(int id)
+        {
+            var dbctx = new CURDBEntities();
+            var record = dbctx.Orders.Find(id);
+            if (record != null)
+                return record;
+            else
+                throw new Exception("Record Not Found....");
+            dbctx.SaveChanges();
+        }
+        public void AddOrder(Order o)
+        {
+            var dbctx = new CURDBEntities();
+            var rescord = dbctx.Orders.Add(o);
+            dbctx.SaveChanges();
+        }
+        /* Delete any order
+        public void DelOrder(int id)
+        {
+            var dbctx = new CURDBEntities();
+            var record = dbctx.Orders.Find(id);
+            dbctx.Orders.Remove(record);
+            dbctx.SaveChanges();
+        }
+        */
     }
 }
